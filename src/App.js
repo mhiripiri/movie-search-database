ce5cc6446e24e013772b653f9bf439b3d4f28fbb
Dominic Mhiripiri <mhiripiri@gmail.com>
2020-07-20 17:07:31 +0200

    added search functionality and connected backend to api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 import React, { Component }  from 'react';
 import './App.css';
 import MovieRow from './MovieRow'
+import $ from 'jquery';
 
 class App extends Component {
   constructor(props) {
     super(props)
+    this.state= {}
 
-    const movies = [
-      {id: 0, poster_src: "", title: "Avengers: Infinity War", overview: "Lorem ipsum dolor esat"},
-      {id: 1, poster_src: "", title: "Terminator", overview: "Lorem ipsum dolor esat"},
+  //   const movies = [
+  //     {id: 0, poster_src: "", title: "Avengers: Infinity War", overview: "Lorem ipsum dolor esat"},
+  //     {id: 1, poster_src: "", title: "Terminator", overview: "Lorem ipsum dolor esat"},
       
-    ]
+  //   ]
     
    
-    var movieRows = [];
-    movies.forEach((movie) => {
-      console.log(movie.title)
+  //   var movieRows = [];
+  //   movies.forEach((movie) => {
+  //     console.log(movie.title)
 
-    const movieRow = <MovieRow movie={movie} />
+  //   const movieRow = <MovieRow movie={movie} />
 
-        movieRows.push(movieRow)
-    })
+  //       movieRows.push(movieRow)
+  //   })
     
-    this.state = {rows: movieRows}
+  //   this.state = {rows: movieRows}
+  // }
+  this.performSearch("england")
+
+}
+
+  performSearch(searchTerm) {
+    console.log("Perform Search using TMdB")
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=973caae44cfe60854211375f4a9dc7d6&language=en-US&page=1&include_adult=false&&query=" + searchTerm;
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("Fetched Successfully")
+        // console.log(searchResults)
+        const results = searchResults.results
+        console.log(results[0])
+
+        var movieRows = []
+
+        results.forEach((movie) => {
+          movie.poster_src = "http://image.tmdb.org/t/p/w185" + movie.poster_path
+          console.log(movie.title)
+          const movieRow = <MovieRow key={movie.id} movie={movie} />
+          movieRows.push(movieRow)
+        })
+
+        this.setState({rows: movieRows})
+    
+      },
+      error: (xhr, status, err) => {
+        console.error("Error: Failed to Fetch Data")
+      }
+    })
+  }
+
+  searchChangeHandler (event) {
+    console.log(event.target.value)
+    const boundObject = this
+    const searchTerm = event.target.value
+    this.performSearch(searchTerm)
   }
 
   render() {
@@ -51,7 +92,7 @@ class App extends Component {
         paddingLeft: 16,
         marginTop: '8px',
         borderRadius: '4px',
-        }} placeholder="Enter your search term..." className="input"
+        }} onChange={this.searchChangeHandler.bind(this)} placeholder="Enter your search term..." className="input"
       />
 
       {this.state.rows}
